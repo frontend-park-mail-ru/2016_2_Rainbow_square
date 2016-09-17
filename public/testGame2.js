94f22d0f9a990e2989c114751158ef2670bb236d
djkah11 <djkah11@yandex.ru>
2016-09-17 12:38:51 +0300

    rework page load7

--- public/testGame2.js
+++ public/testGame2.js
@@ -1,35 +1,34 @@
 /**
  * Created by alexey on 16.09.16.
  */
-if (typeof exports === "object") {}
-else{
+if (typeof exports === "object") {
+}
+else {
     window.onload = function () {
         var h = 4;
         var w = 4;
         var stateCount = 3;
         var cells = [];
-
-        window.onload = function () {
-            var game = document.getElementById("game");
-            game.style.width = game.style.height = "400px";
-            for (var i = 0; i < h; i++) {
-                cells[i] = [];
-                for (var j = 0; j < w; j++) {
-                    var cell = document.createElement('div');
-                    cell.className = 'cell cell0';
-                    // cell.innerHTML = 'g';
-                    cell.id = cellName(j, i);
-                    (function () {
-                        var x = j, y = i;
-                        cell.onclick = function () {
-                            onClick(x, y);
-                        };
-                    })();
-                    game.appendChild(cell);
-                    cells[i][j] = 0;
-                }
+        var game = document.getElementById("game");
+        game.style.width = game.style.height = "400px";
+        for (var i = 0; i < h; i++) {
+            cells[i] = [];
+            for (var j = 0; j < w; j++) {
+                var cell = document.createElement('div');
+                cell.className = 'cell cell0';
+                // cell.innerHTML = 'g';
+                cell.id = cellName(j, i);
+                (function () {
+                    var x = j, y = i;
+                    cell.onclick = function () {
+                        onClick(x, y);
+                    };
+                })();
+                game.appendChild(cell);
+                cells[i][j] = 0;
             }
-        };
+        }
+
 
         function cellName(x, y) {
             return 'cell' + y + '-' + x;
@@ -55,5 +54,8 @@ else{
             cells[y][x] = (cells[y][x] + 1) % stateCount;
             cell.classList.add(stateName(cells[y][x]));
         };
+
+        console.log("INTRO");
     };
 }
+console.log('lol');
