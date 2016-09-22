dc97b1ae82382091b7db6de53fa23b8151401b1f
djkah11 <djkah11@yandex.ru>
2016-09-22 23:51:51 +0300

    Refactor by linter

--- public/main.js
+++ public/main.js
@@ -1,103 +1,102 @@
-'use strict';
-
 let userData = {};
 
-function onLogin(form, block) {
-    userData = {
-        user: form.elements['user'].value,
-        email: form.elements['email'].value
-    };
+function onLogin(form) {
+  userData = {
+    user: form.elements.user.value,
+    email: form.elements.email.value,
+  };
 
-    jsLogin.hidden = true;
-    jsChat.hidden = false;
+  jsLogin.hidden = true;
+  jsChat.hidden = false;
 
 
-    if (userData.user) {
-        jsTitle.innerHTML = jsTitle.innerHTML.replace('%username%', userData.user);
-    }
+  if (userData.user) {
+    jsTitle.innerHTML = jsTitle.innerHTML.replace('%username%', userData.user);
+  }
 
-    subscribe();
+  subscribe();
 }
 
 function createMessage(opts, isMy = false) {
-    let message = document.createElement('div');
-    let email = document.createElement('div');
+  const message = document.createElement('div');
+  const email = document.createElement('div');
 
-    message.classList.add('chat__message');
-    email.classList.add('chat__email');
+  message.classList.add('chat__message');
+  email.classList.add('chat__email');
 
-    if (isMy) {
-        message.classList.add('chat__message_my');
-    } else {
-        message.style.backgroundColor = `#${technolibs.colorHash(opts.email || '')}`;
-    }
-    message.innerHTML = opts.message;
-    email.innerHTML = opts.email;
-    message.appendChild(email);
+  if (isMy) {
+    message.classList.add('chat__message_my');
+  } else {
+    message.style.backgroundColor = `#${technolibs.colorHash(opts.email || '')}`;
+  }
+  message.innerHTML = opts.message;
+  email.innerHTML = opts.email;
+  message.appendChild(email);
 
-    return message;
+  return message;
 }
 
 function onChat(form) {
-    let data = {
-        message: form.elements['message'].value,
-        email: userData.email
-    };
+  const data = {
+    message: form.elements.message.value,
+    email: userData.email,
+  };
 
-    let result = technolibs.request('/api/messages', data);
-    form.reset();
+  const result = technolibs.request('/api/messages', data);
+  form.reset();
 }
 
 function renderChat(items) {
-    jsMessages.innerHTML = '';
-    items.forEach(item => {
-        let message = createMessage(item, item.email === userData.email);
-        jsMessages.appendChild(message);
-    });
-    jsMessages.scrollTop = jsMessages.scrollHeight;
+  jsMessages.innerHTML = '';
+  items.forEach((item) => {
+    const message = createMessage(item, item.email === userData.email);
+    jsMessages.appendChild(message);
+  });
+  jsMessages.scrollTop = jsMessages.scrollHeight;
 }
 
 function subscribe() {
-    technolibs.onMessage(data => {
-        renderChat(Object.keys(data).map(key => data[key]));
-    });
+  technolibs.onMessage((data) => {
+    renderChat(Object.keys(data).map(key => data[key]));
+  });
 }
 
 function hello(text) {
-    return 'Привет, ' + text;
-}
-
-if (typeof exports === "object") {
-    exports.hello = hello;
-    exports.plural = plural;
-    exports.filter = filter;
+  return 'Привет, ' + text;
 }
 
 function plural(number, language) {
-    let parsedNumber = (parseInt(number));
-    if (parsedNumber) {
-        for (let key in language) {
-            if ((language[key][0](parsedNumber)))
-                return number + ' ' + language[key][language[key].length - 1];
-        }
+  const parsedNumber = (parseInt(number));
+  if (parsedNumber) {
+    for (const key in language) {
+      if ((language[key][0](parsedNumber))) {
+        return number + ' ' + language[key][language[key].length - 1];
+      }
     }
-    return number + ' ' + language.last[2];
+  }
+  return number + ' ' + language.last[2];
 }
 
 function filter(str = '') {
-    let rules = window.rules;
-    //очищаем слова от пробелов и прочего
-    str += '';
-    rules = rules.map(rule => {
-        return {
-            regex: RegExp(`\\b${rule}\\b`, 'g'),
-            length: rule.length
-        }
-    });
+  let rules = window.rules;
+    // очищаем слова от пробелов и прочего
+  str += '';
+  rules = rules.map((rule) => {
+    return {
+      regex: RegExp(`\\b${rule}\\b`, 'g'),
+      length: rule.length,
+    };
+  });
 
-    rules.forEach(rule => {
-        str = str.replace(rule.regex, new Array(rule.length + 1).join('*'));
-    });
+  rules.forEach((rule) => {
+    str = str.replace(rule.regex, new Array(rule.length + 1).join('*'));
+  });
 
-    return str;
+  return str;
+}
+
+if (typeof exports === 'object') {
+  exports.hello = hello;
+  exports.plural = plural;
+  exports.filter = filter;
 }
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-let express = require('express');
-let parser = require('body-parser');
-let app = express();
-let technoDoc = require('techno-gendoc');
-let path = require('path');
+const express = require('express');
+const parser = require('body-parser');
 
-let technolibs = require('technolibs');
+const app = express();
+const technoDoc = require('techno-gendoc');
+
+const technolibs = require('technolibs');
 
 app.use('/', express.static('public'));
 technoDoc.generate(require('./api'), 'public');
@@ -13,30 +13,31 @@ app.use(parser.json());
 app.use('/libs', express.static('node_modules'));
 
 app.post('/api/messages', (req, res) => {
-	technolibs.publish(req.body).then(body => res.json(req.body));
+  technolibs.publish(req.body).then(body => res.json(req.body));
 });
 
-app.get('/api/messages', function (req, res) {
-	res.send([
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message'))
-	])
+app.get('/api/messages', (req, res) => {
+  res.send([
+    technoDoc.mock(require('./api/scheme/Message')),
+    technoDoc.mock(require('./api/scheme/Message')),
+    technoDoc.mock(require('./api/scheme/Message')),
+    technoDoc.mock(require('./api/scheme/Message')),
+  ]);
 });
 
-let requestCountMap = new Map();
+const requestCountMap = new Map();
 app.post('/users', (req, res) => {
-	console.log(req.body);
-	var email = req.body.email;
-	var count = 1;
-	if (requestCountMap.has(email)) {
-		count = requestCountMap.get(email) + 1;
-	}
-	requestCountMap.set(email, count);
-	res.send(count.toString());
+  console.log(req.body);
+  const email = req.body.email;
+  let count = 1;
+  if (requestCountMap.has(email)) {
+    count = requestCountMap.get(email) + 1;
+  }
+
+  requestCountMap.set(email, count);
+  res.send(count.toString());
 });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`App started on port ${process.env.PORT || 3000}`);
+  console.log(`App started on port ${process.env.PORT || 3000}`);
 });
