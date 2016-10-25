5bcc3ebb71acaaa4c72faa528a4aa404df2b9b53
djkah11 <djkah11@yandex.ru>
2016-10-25 18:20:06 +0300

    update api

--- api/index.js
+++ api/index.js
@@ -1,20 +1,22 @@
 module.exports = {
-	"swagger": "2.0",
-	"info": {
-		"version": "0.0.3",
-		"title": "TechnoChat API",
-		"description": "**ТехноЧат**"
-	},
-	"basePath": "/api",
-	"schemes": ["http"],
-   	"host": "http://localhost:3000",
+  'swagger': '2.0',
+  'info': {
+    'version': '0.0.3',
+    'title': 'TechnoChat API',
+    'description': '**ТехноЧат**',
+  },
+  'basePath': '/api',
+  'schemes': ['http'],
+  'host': 'http://localhost:3000',
 
-	paths: {
-		'/messages': require('./resources/messages')
-	},
+  paths: {
+    '/session': require('./resources/session'),
+    '/user': require('./resources/user'),
+  },
 
-	definitions: {
-		Message: require('./scheme/Message'),
-	}
+  definitions: {
+    Session: require('./scheme/Session'),
+    User: require('./scheme/User'),
+  },
 
-}
+};
